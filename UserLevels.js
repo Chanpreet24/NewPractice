bd6d2fb4705accb3f5f115904afcbafb96d67083
Chanpreet24 <62950691+Chanpreet24@users.noreply.github.com>
2021-09-17 15:34:16 +0100

    User Level accordian JS congiuration created and linked to userLevel HTML

---
+++ UserLevels.js
@@ -0,0 +1,10 @@
+var accordian = document.getElementsByClassName('contentBox');
+
+document.querySelector('#back-button').addEventListener('click', () => {
+    window.location.href = './index.html';
+});
+
+
+for (i = 0; i < accordian.length; i++){
+    accordian[i].addEventListener('click', function(){this.classList.toggle('active')})
+}
